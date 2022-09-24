bb4496d9ba49ea4323efc47a297938fd5d670496
Matheus Pazinati <matheuspazinati@gmail.com>
2022-09-24 08:49:49 -0300

    Cria funcionalidade de marcar a tarefa como concluida

--- src/components/CreateTasks.tsx
+++ src/components/CreateTasks.tsx
@@ -1,15 +1,75 @@
 // import { EmptyList } from './EmptyList'
+import { ChangeEvent, FormEvent, useState } from 'react'
+
 import { Task } from './Task'
 
 import styles from './CreateTasks.module.css'
 import { PlusCircle } from 'phosphor-react'
 
+interface NewTaskProps {
+  content: string;
+  isComplete: boolean;
+}
 
 export function CreateTasks() {
+const [tasks, setTasks] = useState<NewTaskProps[]>([])
+
+const [newTask, setNewTask] = useState<NewTaskProps>({content: '', isComplete: false})
+
+function handleNewTask(event: ChangeEvent<HTMLInputElement> ) {
+  event.preventDefault()
+  setNewTask({
+    content: event.target.value,
+    isComplete: false
+  })
+}
+
+function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault()
+  setTasks([...tasks, newTask])
+  setNewTask({
+    content: '',
+    isComplete: false
+  })
+}
+
+function checkTask(id: string) {
+  const tasksChecked = tasks.map((task) => {
+    if (task.content === id) {
+      return (
+        {
+          content: task.content,
+          isComplete: !task.isComplete
+        }
+      )
+    } else {
+      return (
+        {
+          content: task.content,
+          isComplete: task.isComplete
+        }
+      )
+    }
+  })
+  setTasks(tasksChecked)
+}
+
+const checkedTasks = tasks.filter((task) => {
+  return task.isComplete === true;
+})
+
   return (
     <div className={styles.container}>
-      <form className={styles.addTask}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
+      <form 
+        className={styles.addTask}
+        onSubmit={handleCreateNewTask}
+      >
+        <input
+         type="text" 
+         placeholder='Adicione uma nova tarefa'
+         value={newTask.content}
+         onChange={handleNewTask}
+        />
         <button type='submit'>
           Criar
           <PlusCircle
@@ -21,17 +81,23 @@ export function CreateTasks() {
 
       <div className={styles.taskStatus}>
         <p className={styles.statusCreated}>
-          <span>Tarefas criadas </span>
-          <span className={styles.statusCount}>0</span>
+          <span>Tarefas criadas</span>
+          <span className={styles.statusCount}>{tasks.length}</span>
         </p>
         <p className={styles.statusDone}>
-          <span>Concluídas </span>
-          <span className={styles.statusCount}>0</span></p>
+          <span>Concluídas</span>
+          <span className={styles.statusCount}>{checkedTasks.length}</span></p>
       </div>
-
-      <Task />
-      <Task />
-      <Task />
+        {tasks.map((task) => {
+          return (
+           <Task 
+            content={task.content} 
+            key={task.content} 
+            completed={task.isComplete}
+            onCheck={checkTask} 
+           />
+          )
+        })}
     </div>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,16 +3,31 @@ import { Check, Trash } from 'phosphor-react'
 
 import styles from './Task.module.css'
 
-export function Task() {
+interface TaskProps {
+  content: string;
+  completed: boolean;
+  onCheck: (content: string) => void
+}
+
+export function Task({ content, completed, onCheck }: TaskProps) {
+
+  function handleCheck() {
+    onCheck(content)
+  }
+
   return (
     <div className={styles.task}>
       <label>
-        <Checkbox.Root className={styles.checkbox}>
+        <Checkbox.Root
+         className={styles.checkbox}
+         checked={completed}
+         onCheckedChange={handleCheck}
+        >
           <Checkbox.Indicator>
             <Check className={styles.iconCheck}/>
           </Checkbox.Indicator>
         </Checkbox.Root>
-        Lorem ipsum dolor sitLorem ipsum dolor
+        {content}
       </label>
       <Trash
         size={24}
